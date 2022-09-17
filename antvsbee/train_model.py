f5f462d5e95214b0c42b6469bee1484a3473103a
edp1096 <edp1096@naver.com>
2022-09-18 02:44:26 +0900

    antvsbee data location rename

--- antvsbee/train_model.py
+++ antvsbee/train_model.py
@@ -1,21 +1,22 @@
 import torch
 import torch.backends.cudnn as cudnn
 import torch.nn as nn
-from torch.utils.data import DataLoader, ConcatDataset
+import torch.utils
+from torch.utils.data import DataLoader
 from torchinfo import summary
+import torchvision
 from torchvision import datasets, models, transforms
-from torchvision.transforms import ToTensor
 
 import modules.fit as fit
 import modules.network as net
 import modules.valid as valid
 
 import matplotlib.pyplot as plt
+import numpy as np
 import random
 
 
-cudnn.benchmark = True
-plt.ion()  # 대화형 모드
+model_fname = "model_resnet.pt"
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print("Device:", device)
@@ -26,57 +27,79 @@ if device == "cuda":
     torch.cuda.manual_seed_all(777)
 
 
+cudnn.benchmark = True
+
 epochs = 5
 batch_size = 100
 learning_rate = 0.001
 
-workers = 4
+transform = {}
+transform["train"] = transforms.Compose(
+    [
+        transforms.RandomResizedCrop(224),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+    ]
+)
+transform["valid"] = transforms.Compose(
+    [
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+    ]
+)
 
-transform = {
-    "train": transforms.Compose(
-        [
-            transforms.RandomResizedCrop(224),
-            transforms.RandomHorizontalFlip(),
-            transforms.ToTensor(),
-            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
-        ]
-    ),
-    "val": transforms.Compose(
-        [
-            transforms.Resize(256),
-            transforms.CenterCrop(224),
-            transforms.ToTensor(),
-            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
-        ]
-    ),
-}
+dataset = {x: datasets.ImageFolder(f"datas/{x}", transform[x]) for x in ["train", "valid"]}
+loaders = {x: DataLoader(dataset[x], batch_size=batch_size, shuffle=True) for x in ["train", "valid"]}
+# x: DataLoader(dataset[x], batch_size=batch_size, shuffle=True, num_workers=workers) for x in ["train", "valid"]
 
-
-dataset = {x: datasets.ImageFolder(f"datas/{x}", transform[x]) for x in ["train", "val"]}
-loaders = {
-    x: DataLoader(dataset[x], batch_size=batch_size, shuffle=True, num_workers=workers)
-    for x in ["train", "val"]
-}
-
-dataset_sizes = {x: len(dataset[x]) for x in ["train", "val"]}
+dataset_sizes = {x: len(dataset[x]) for x in ["train", "valid"]}
 class_names = dataset["train"].classes
+
+# plt.ion()  # 대화형 모드
+
+
+def imshow(inp, title=None):
+    for i in range(4):
+        inp = images[i].numpy().transpose((1, 2, 0))
+        mean = np.array([0.485, 0.456, 0.406])
+        std = np.array([0.229, 0.224, 0.225])
+
+        inp = std * inp + mean
+        inp = np.clip(inp, 0, 1)
+
+        sp = plt.subplot(1, 4, i + 1)
+        sp.axis("Off")
+
+        plt.title(class_names[classes[i]])
+
+        sp.imshow(inp)
+
+    plt.show()
+
+
+images, classes = next(iter(loaders["train"]))
+out = torchvision.utils.make_grid(images)
+
+imshow(out, title=[class_names[x] for x in classes])
 
 
 # resnet
-model = models.resnet18()
+model = models.resnet18(weights=models.ResNet18_Weights.IMAGENET1K_V1)
 model.conv1 = nn.Conv2d(1, 64, kernel_size=(7, 7), stride=(1, 1), padding=(3, 3), bias=False)
 model.fc = nn.Linear(512, 10)
 
 model.to(device)
 print(model)
 
-exit()
 
 # summary(model, input_size=(batch_size, 1, 28 * 28))  # linear. 모델 정보 출력 (channels, height, width)
 summary(model, input_size=(batch_size, 1, 28, 28))  # cnn
 # summary(model, input_size=(batch_size, 1, input_size))  # rnn, lstm
 
-total_batch = len(train_loader)
+total_batch = len(loaders["train"])
 print("총 배치의 수 : {}".format(total_batch))
 
 criterion = nn.CrossEntropyLoss().to(device)  # 비용 함수에 소프트맥스 함수 포함되어져 있음
@@ -88,17 +111,9 @@ optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 for epoch in range(epochs):
     print(f"Epoch {epoch+1}\n-------------------------------")
 
-    # linear
-    # fit.run(device, train_loader, model, criterion, optimizer)
-    # valid.run(device, valid_loader, model, criterion)
-
     # cnn
-    fit.runCNN(device, train_loader, model, criterion, optimizer)
-    valid.runCNN(device, valid_loader, model, criterion)
-
-    # rnn, lstm
-    # fit.runRNN(device, train_loader, model, criterion, optimizer)
-    # valid.runRNN(device, valid_loader, model, criterion)
+    fit.runCNN(device, loaders["train"], model, criterion, optimizer)
+    valid.runCNN(device, loaders["valid"], model, criterion)
 
 print("Training done!")
 
