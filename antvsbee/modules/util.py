9876b08e451ec0f8579f21fb519797595f8cf84a
edp1096 <edp1096@naver.com>
2022-09-27 16:16:15 +0900

    print

--- antvsbee/modules/util.py
+++ antvsbee/modules/util.py
@@ -9,7 +9,7 @@ def printModelInfo(model, batch_size, loader):
     summary(model, input_size=(batch_size, 3, 7, 7))
 
     total_batch = len(loader)
-    print("총 배치 수 : {}".format(total_batch))
+    print("Batch count : {}".format(total_batch))
 
 
 def imshow(images, predicts):
