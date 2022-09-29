2e0065a5c45d08300953471528bab5d760314502
edp1096 <edp1096@naver.com>
2022-09-30 00:47:41 +0900

    sentiment

---
+++ torchtext/sentiment.py
@@ -0,0 +1,2 @@
+# https://yeong-jin-data-blog.tistory.com/entry/BERT%EB%A1%9C-%EC%98%81%ED%99%94-%EB%A6%AC%EB%B7%B0-%EA%B0%90%EC%84%B1-%EB%B6%84%EC%84%9D%ED%95%98%EA%B8%B0
+# http://yonghee.io/bert_binary_classification_naver
